c2bea1d460c70f2bbb3a43f71814c5b39c6a15ac
dom <dom@domlib.com>
2015-11-12 09:17:42 +0800

    使用shader内的矩阵乘法

--- src/shaders/shader-vs.glsl
+++ src/shaders/shader-vs.glsl
@@ -14,11 +14,10 @@ attribute float ty;
 varying vec2 v_TexCoord;
 varying float v_Alpha;
 void main(){
-    float x = a * a_Position.x + c * a_Position.y + tx;
-    float y = b * a_Position.x + d * a_Position.y + ty;
-    vec2 position = vec2(x,y);
+    mat3 matrix = mat3(a,b,0,c,d,0,tx,ty,1);
+    vec3 position = matrix*vec3(a_Position,1);
     //转换坐标到（-1，1）
-    vec2 clipSpace = (position / u_ScreenSize) * 2.0 - 1.0;
+    vec2 clipSpace = (position.xy / u_ScreenSize) * 2.0 - 1.0;
     //反转Y轴
     clipSpace = clipSpace * vec2(1, -1);
 
